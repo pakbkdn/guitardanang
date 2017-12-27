710e20f87c19d953297f336336979ea657264bc4
nguyenxuanphuong1211 <nguyenxuanphuong1211@gmail.com>
2017-12-27 17:13:37 +0700

    add_mail

--- config/mail.php
+++ config/mail.php
@@ -56,9 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
-    ],
+        'address' => 'guitardn189@gmail.com', 'name' => 'Guitar Đà Nẵng'],
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +82,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => 'guitardn189@gmail.com',
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => 'p123456789',
 
     /*
     |--------------------------------------------------------------------------
@@ -118,6 +116,13 @@ return [
         'paths' => [
             resource_path('views/vendor/mail'),
         ],
+    ],
+    'stream' => [
+    'ssl' => [
+        'allow_self_signed' => true,
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+    ],
     ],
 
 ];
